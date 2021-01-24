0c47f470c3e947306d281bb25b15a245012df3cd
larsolib <larsolib@stud.ntnu.no>
2021-01-24 16:21:46 +0100

    make a mockup of home page

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF000000);
+const kSecondaryColor = Color(0xFFE5E5E5);
+
+const kTextColor = Color(0xFFFFFFFF);
+
+
+const kHeadingextStyle = TextStyle(
+  fontSize: 10,
+  color: kTextColor,
+  //fontWeight: FontWeight.bold,
+);
